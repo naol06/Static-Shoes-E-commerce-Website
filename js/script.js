4dceddffc21592d9ca310a694dcfee05fcab84a5
naol06 <118481867+naol06@users.noreply.github.com>
2023-12-22 16:57:18 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// $(document).ready(function(){
+//     $(".imm").mouseenter(function(){
+//        let zoom=$(".imm");
+    
+//     zoom.animate({width: '+=50px', opacity: '1'}, "slow");
+   
+//     })
+   
+//     $(".cc").click(function(){
+//         $(".imm").stop();
+//     });
+// })
